8bd6962e513a981da798416f3caf465a6c44621e
Jason Chen <cfg815@gmail.com>
2021-04-15 16:45:42 -0400

    static files to deploy

---
+++ dist/js/chunk-2d2163ba.481ec393.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2163ba"],{c21f:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("transition",{attrs:{name:"fade"}},[t("router-view")],1)},r=[],u={},c=u,s=t("2877"),i=Object(s["a"])(c,a,r,!1,null,null,null);e["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d2163ba.481ec393.js.map
